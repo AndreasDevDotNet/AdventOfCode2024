a2d92c464c03ef0bbfd2494896e4f8ce3180fb32
Kristiansen, Andreas <andreas.kristiansen@emerson.com>
2024-11-25 00:31:30 +0100

    Add project files.

---
+++ ScratchPad/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- Day XX: ScratchPad ---");
+var input = File.ReadAllLines("input.txt").ToList().Select(x => ParseInput(x)).ToList();
+
+
+
+Console.WriteLine($"Part 1: ");
+Console.WriteLine($"Part 2: ");
+
+static string ParseInput(string input)
+{
+    return input;
+}
