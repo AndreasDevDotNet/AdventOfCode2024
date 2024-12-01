32b3182af208c02a0b8aabc2aca7c7464bffc961
Kristiansen, Andreas <andreas.kristiansen@emerson.com>
2024-12-01 07:28:01 +0100

    Added day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿using AoCToolbox;
+
+Console.WriteLine("--- Day 1: Historian Hysteria ---");
+var input = File.ReadAllLines("input.txt").ToList();
+
+var leftList = new List<int>();
+var rightList = new List<int>();
+foreach (var item in input)
+{
+    var parts = item.ParseInts();
+    leftList.Add(parts[0]);
+    rightList.Add(parts[1]);
+}
+
+leftList.Sort();
+rightList.Sort();
+
+var sumDist = 0;
+var numSimmilar = 0;
+for (int i = 0; i < leftList.Count; i++)
+{
+    sumDist += Math.Max(rightList[i], leftList[i]) - Math.Min(leftList[i],rightList[i]);
+    var rightListCount = rightList.Count(x => x == leftList[i]);
+    numSimmilar += leftList[i] * rightListCount;
+}
+
+Console.WriteLine($"Part 1: {sumDist}");
+Console.WriteLine($"Part 2: {numSimmilar}");
+
+
+
